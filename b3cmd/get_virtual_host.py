c214e311a621fe9199993cfd1c9d987ab16922fa
Mike Wyatt <wyatt.mike@gmail.com>
2016-04-01 09:49:00 -0300

    prepend namespace to domain name if it's not the default

--- b3cmd/get_virtual_host.py
+++ b3cmd/get_virtual_host.py
@@ -16,4 +16,13 @@ def get_virtual_host(default):
     except BaseException:
         pass
 
-    return virtual_host_custom or virtual_host_simple or default
+    return prepend_namespace(
+        virtual_host_custom or virtual_host_simple or default
+    )
+
+
+def prepend_namespace(host):
+    if api.env.namespace != api.env.namespace_from_server:
+        return ('%(namespace)s--' % api.env) + host
+
+    return host
